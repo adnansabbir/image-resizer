500705a261d6d0c3c490466f669d8be13c4f3920
Adnan Sabbir <adnan.sabbir@shohoz.com>
2021-05-03 18:40:10 +0600

    Implemented S3 bucket and returning presigned urls for keys

---
+++ server/src/aws/s3-bucket.ts
@@ -0,0 +1,31 @@
+import AWS from 'aws-sdk';
+
+AWS.config.getCredentials(function (err) {
+    if (err) throw new Error('Error getting aws config');
+});
+AWS.config.update({region: 'us-east-1'});
+const s3 = new AWS.S3();
+
+async function getSignedUrl(key: string) {
+    return new Promise((resolve, reject) => {
+        let params = {Bucket: 'im-homework', Key: key};
+        s3.getSignedUrl('getObject', params, (err, url) => {
+            if (err) reject(err);
+            resolve(url);
+        });
+    });
+}
+
+async function getSignedUrls(keys: string[]) {
+    const urls = [];
+    for (let key of keys) {
+        const signedUrl = await getSignedUrl(key);
+        urls.push({
+            key,
+            url: signedUrl
+        });
+    }
+    return urls;
+}
+
+export {getSignedUrls};
