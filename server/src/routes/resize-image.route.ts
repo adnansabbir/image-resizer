15e057d8270dfe1c9890b853403488633cd54097
adnan <adnan.sabbir@gmail.com>
2021-05-06 10:01:34 +0600

    Error handled and sending urls as response

--- server/src/routes/resize-image.route.ts
+++ server/src/routes/resize-image.route.ts
@@ -7,6 +7,10 @@ const router = express.Router();
 router.post('/resize-images', async (req, res, next) => {
     try {
         const filesDataToResize = req.body;
+        if (filesDataToResize.length > 5) {
+            res.status(400).send('Maximum 5 images can be converted');
+            return;
+        }
         const fileDataWithUrls: SQSMessageBody[] = [];
         for (let fd of filesDataToResize) {
             const preSignedUrl = await awsService.getSignedUrl(fd.fileName);
@@ -16,8 +20,8 @@ router.post('/resize-images', async (req, res, next) => {
                 fileName: fd.fileName
             })
         }
-        await awsService.sendResizeTaskToQueue(fileDataWithUrls);
-        res.status(200).send('Task started');
+        const urls = await awsService.sendResizeTaskToQueue(fileDataWithUrls);
+        res.status(200).send(fileDataWithUrls);
     } catch (err) {
         res.status(400).send('An error occurred');
     }
